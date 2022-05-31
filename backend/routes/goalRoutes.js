44d175c9107a40e1d423b45aa5acf724d23853f1
Istip <isticsek@gmail.com>
2022-05-31 20:35:59 +0300

    Added protected routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,12 +5,13 @@ const {
   editGoal,
   deleteGoal,
 } = require('../controllers/goalController');
+const { protect } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.get('/', getGoals);
-router.post('/', createGoal);
-router.put('/:id', editGoal);
-router.delete('/:id', deleteGoal);
+router.get('/', protect, getGoals);
+router.post('/', protect, createGoal);
+router.put('/:id', protect, editGoal);
+router.delete('/:id', protect, deleteGoal);
 
 module.exports = router;
